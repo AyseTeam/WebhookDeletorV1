04822c8530dacd888460ac5fc04fe5a9157b43c1
AyseTeam <86293015+AyseTeam@users.noreply.github.com>
2021-06-22 14:56:26 +0200

    Add files via upload

---
+++ WebhookDeleterV1/Program.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Specialized;
+using System.IO;
+using System.Net;
+using System.Text;
+using Figgle;
+using System.Threading;
+
+namespace WebhookDeletorV1
+{
+	internal class Program
+	{
+		public static void Main(string[] args)
+		{
+			// Console.SetWindowSize(93, 19);
+			Console.ForegroundColor = ConsoleColor.Blue;
+			Console.Title = "[ DISCORD ] Webhook deleter / LOADING ...";
+
+			Console.WriteLine(FiggleFonts.Rectangles.Render("WEBHOOK-DELETOR"));
+
+			Console.Title = "[ DISCORD ] Webhook deleter";
+			Console.ForegroundColor = ConsoleColor.Red;
+			Console.Write("\n\nWebhook : ");
+			string str = Console.ReadLine();
+			Console.ForegroundColor = ConsoleColor.Yellow;
+			string webhookurl = str;
+
+			
+
+			Program.deletewebhook(webhookurl, "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.121 Safari/537.36");
+			
+			Thread.Sleep(700);
+
+			Console.Title = "[ DISCORD ] Webhook deleter / LOADING ...";
+
+			Thread.Sleep(700);
+
+			Console.Clear();
+			Console.Title = "[ DISCORD ] Webhook deleter / SUCCESS";
+			Console.ForegroundColor = ConsoleColor.Magenta;
+			Console.WriteLine(" ___ _   _  ___ ___ ___  ___ ___  ");
+			Console.WriteLine("/ __| | | |/ __/ __/ _ \\/ __/ __|");
+			Console.WriteLine("\\__ \\ |_| | (_| (_|  __/\\__ \\__ \\  ");
+			Console.WriteLine("|___/\\____|\\___\\___\\___||___/___/ ");
+			Console.ForegroundColor = ConsoleColor.Green;
+			Thread.Sleep(100);
+			Console.Write("\n WebHook : Good Deleted");
+			Console.ReadKey();
+		}
+
+		public static string deletewebhook(string webhookurl, string UserAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.121 Safari/537.36")
+		{
+			string result;
+			try
+			{
+				HttpWebRequest httpWebRequest = (HttpWebRequest)WebRequest.Create(webhookurl);
+				byte[] bytes = Encoding.ASCII.GetBytes("{}");
+				httpWebRequest.Method = "DELETE";
+				httpWebRequest.UserAgent = UserAgent;
+				httpWebRequest.ContentLength = (long)bytes.Length;
+				using (Stream requestStream = httpWebRequest.GetRequestStream())
+				{
+					requestStream.Write(bytes, 0, bytes.Length);
+				}
+				HttpWebResponse httpWebResponse = (HttpWebResponse)httpWebRequest.GetResponse();
+				result = new StreamReader(httpWebResponse.GetResponseStream()).ReadToEnd();
+			}
+			catch (Exception ex)
+			{
+				Console.Clear();
+				result = ex.ToString();
+				Console.Write("Webhook invalid");
+				
+					Console.Beep();
+					Console.Beep();
+					Console.Beep();
+
+				Console.ReadKey();
+				Environment.Exit(0);
+			}
+			return result;
+		}
+
+		private static byte[] Post(string uri, NameValueCollection pairs)
+		{
+			byte[] result;
+			using (WebClient webClient = new WebClient())
+			{
+				result = webClient.UploadValues(uri, pairs);
+			}
+			return result;
+		}
+	}
+}
---
+++ WebhookDeleterV1/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Les informations générales relatives à un assembly dépendent de
+// l'ensemble d'attributs suivant. Changez les valeurs de ces attributs pour modifier les informations
+// associées à un assembly.
+[assembly: AssemblyTitle("WebhookDeletorV1")]
+[assembly: AssemblyDescription("Delete a webhook discord")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("EZ Inc.")]
+[assembly: AssemblyProduct("WebhookDeletorV1")]
+[assembly: AssemblyCopyright("Copyright © EZ Inc. 2021")]
+[assembly: AssemblyTrademark("EaZy")]
+[assembly: AssemblyCulture("")]
+
+// L'affectation de la valeur false à ComVisible rend les types invisibles dans cet assembly
+// aux composants COM. Si vous devez accéder à un type dans cet assembly à partir de
+// COM, affectez la valeur true à l'attribut ComVisible sur ce type.
+[assembly: ComVisible(false)]
+
+// Le GUID suivant est pour l'ID de la typelib si ce projet est exposé à COM
+[assembly: Guid("7a11503d-d181-4175-a778-db3fef52c5b5")]
+
+// Les informations de version pour un assembly se composent des quatre valeurs suivantes :
+//
+//      Version principale
+//      Version secondaire
+//      Numéro de build
+//      Révision
+//
+// Vous pouvez spécifier toutes les valeurs ou indiquer les numéros de build et de révision par défaut
+// en utilisant '*', comme indiqué ci-dessous :
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
